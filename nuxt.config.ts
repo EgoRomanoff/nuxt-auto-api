f8e3d79fd93647f0ad5eb07003657e6efbf7933e
EgoRomanoff <egoromanoff@mail.ru>
2025-07-03 09:53:16 +0300

    start to create config and folder structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+    compatibilityDate: '2025-05-15',
+    devtools: { enabled: true },
+
+    plugins: [
+      '~/plugins/api/01.index.ts',
+    ]
 })
